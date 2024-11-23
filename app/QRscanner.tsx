7f1550197b094e7f0777b4477055b55dbb4cbc4c
TuftyRairu <craususrhyle@gmail.com>
2024-11-23 00:43:27 -1100

    adding QRScanner

---
+++ app/QRscanner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function QRscanner() {
+    return (
+        <View>this is the first landing do not touch</View>
+    );
+}
+
+export default QRscanner;
