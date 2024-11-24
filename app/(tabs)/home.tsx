38469e242b5315092ec728f180a2e960ac1ed6dc
yourMACARONi <craususrhyle@gmail.com>
2024-11-23 19:32:38 -1100

    fixing home and adding the fidash

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+import FiDash from "@/components/FiDash";
 
-function home() {
-    return (
-        <View>
-            <Text>
-                This is the home
-            </Text>
-        </View>
-    );
+export default function home() {
+  return (
+    <View style={styles.container}>
+      <FiDash></FiDash>
+    </View>
+  );
 }
 
-export default home;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
