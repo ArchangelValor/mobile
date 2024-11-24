e992ede9b6a0f30bc5c4ed7662e63f55914e91a0
yourMACARONi <craususrhyle@gmail.com>
2024-11-23 19:09:47 -1100

    adding home and rscanner on tabs

---
+++ app/(tabs)/rscanner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function rscanner() {
+    return (
+        <View>
+            <Text>this is the receipt scanner screen </Text>
+        </View>
+    );
+}
+
+export default rscanner;
